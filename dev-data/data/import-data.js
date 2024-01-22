cfd0cde518cc1706d5cac324332feef5b05fced4
Anmol Singh <anmol.sihra890@gmail.com>
2024-01-22 17:55:45 +0530

    inital commit

---
+++ dev-data/data/import-data.js
@@ -0,0 +1,59 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
+
+dotenv.config({ path: './config.env' }); // always on top
+const Db = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
+
+mongoose
+  .connect(Db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log('conncected to database');
+  });
+
+// Reading json
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
+
+//Import data in db
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
+    console.log('data successfully CREATED');
+  } catch (err) {
+    console.error(err);
+  }
+  process.exit();
+};
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('data successfully DELETED');
+  } catch (err) {
+    console.error(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
